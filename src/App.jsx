5faf6c73fbd4c83afbdf4b0ac7c7d787cdbf623b
Phillip Friedel <s0557917@htw-berlin.de>
2023-02-18 17:16:39 +0100

    Added context and control to settings variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import Timer from "./components/Timer"
 import Settings from "./components/Settings"
 import { useState } from "react"
+import SettingsContext from "./components/SettingsContext"
 
 function App() {
 
-  const [showSettings, setShowSettings] = useState(true) 
+  const [showSettings, setShowSettings] = useState(false) 
+  const [workMinutes, setWorkMinutes] = useState(45)
+  const [breakMinutes, setBreakMinutes] = useState(15)
 
   return (
     <main >
-      {showSettings ? <Settings /> : <Timer />}
+      <SettingsContext.Provider value={{
+        workMinutes,
+        breakMinutes,
+        setWorkMinutes,
+        setBreakMinutes,
+      }}>
+        {showSettings ? <Settings /> : <Timer />}
+      </SettingsContext.Provider>
     </main>
   )
 }
--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,12 +1,29 @@
+import { useContext } from "react";
+import SettingsContext from "./SettingsContext";
 
 export default function Settings() {
+
+    const settingsInfo = useContext(SettingsContext);
+
     return(
-        <div className="text-left">
-            <label className="block">Work Minutes:</label>
-            <input type={"number"}></input>
-            
-            <label className="block">Break Minutes:</label>
-            <input type={"number"}></input>
+        <div className="text-left space-y-1">
+            <label className="block">Work Minutes: <span className="font-bold">{settingsInfo.workMinutes}:00</span></label>
+            <input 
+                className="text-black rounded-sm py-[1px] px-[2px]" 
+                type={"number"}
+                value={settingsInfo.workMinutes}
+                onChange={(newValue) => settingsInfo.setWorkMinutes(newValue.target.value)}
+            >
+            </input>
+
+            <label className="block">Break Minutes: <span className="font-bold">{settingsInfo.breakMinutes}:00</span></label>
+            <input 
+                className="text-black rounded-sm py-[1px] px-[2px]"
+                type={"number"} 
+                value={settingsInfo.breakMinutes}
+                onChange={(newValue) => settingsInfo.setBreakMinutes(newValue.target.value)}
+            >
+            </input>
         </div>
     )
 }
