9448df72e08b603726d6c87b0ac9327e3b4c3a51
Phillip Friedel <s0557917@htw-berlin.de>
2023-02-18 17:01:43 +0100

    Set up basic settings structure

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,8 +1,12 @@
+
 export default function Settings() {
     return(
         <div className="text-left">
             <label className="block">Work Minutes:</label>
+            <input type={"number"}></input>
+            
             <label className="block">Break Minutes:</label>
+            <input type={"number"}></input>
         </div>
     )
 }
